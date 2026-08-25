921606e31645575498208d20f470831ad23918d9
Seungwan Ryu <ryusw007@gmail.com>
2026-08-25 23:26:23 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='hohasa-2026-08-24';
+const C='hohasa-2026-08-25';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
 self.addEventListener('fetch',e=>{const req=e.request; if(req.method!=='GET') return;
